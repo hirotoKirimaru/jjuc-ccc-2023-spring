28d83d3c59cf5ccae8366ef8b23d003fc42602c4
水上 皓登 <nainaistar@gmail.com>
2023-04-15 23:43:35 +0900

    first commit

---
+++ src/main/java/test/jjug_ccc_2023_spring/JjugCcc2023SpringApplication.java
@@ -0,0 +1,13 @@
+package test.jjug_ccc_2023_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjugCcc2023SpringApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JjugCcc2023SpringApplication.class, args);
+  }
+
+}
