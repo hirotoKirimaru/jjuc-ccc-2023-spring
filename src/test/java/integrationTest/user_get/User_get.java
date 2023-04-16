8ade13a11a56f907ff3fba9aef6ef4afc90e628a
水上 皓登 <nainaistar@gmail.com>
2023-04-17 00:10:46 +0900

    ComponentScan

--- src/test/java/integrationTest/user_get/User_get.java
+++ src/test/java/integrationTest/user_get/User_get.java
@@ -7,6 +7,7 @@ import java.net.URI;
 import java.util.List;
 import kirimaru.api.ControllerConstant;
 import kirimaru.api.ControllerConstant.Uri;
+import kirimaru.api.UsersApi;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -17,11 +18,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.WebApplicationContext;
 
 //public class User_get extends IntegrationTestsTemplate {
@@ -29,11 +35,23 @@ import org.springframework.web.context.WebApplicationContext;
 
 //@SpringBootTest
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ComponentScan(basePackages = "kirimaru") // プロダクトコードをDIさせるために必要
+//@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+
+//@SpringBootTest
 @SpringBootConfiguration
 @EnableAutoConfiguration
 // RANDOM_PORTで起動させるために必要  https://stackoverflow.com/questions/48264118/spring-boot-test-fails-saying-unable-to-start-servletwebserverapplicationcontex
-@Disabled("結合試験？")
+//@Disabled("結合試験？")
 public class User_get {
+//  @Configuration
+//  public class TestCustomizer {
+//    @Bean
+//    public ServletWebServerFactory serverSettings() {
+//      TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory("/api/v1", port);
+//      return factory;
+//    }
+//  }
 //  @Autowired
 //  private WebApplicationContext context;
 
@@ -47,24 +65,37 @@ public class User_get {
   @LocalServerPort
   private int port;
 
+  //  @Autowired
+//  RestTemplate restTemplate;
   @Autowired
   TestRestTemplate restTemplate;
+//  @Autowired
+//  RestTemplate restTemplate;
 
   @Test
   void test_01() {
-    var responseEntity = get(Uri.USERS);
+//    TestRestTemplate restTemplate = new TestRestTemplate();
+//    var restTemplate = new RestTemplate();
 
-    assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+//    ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8080/api/v1/users", String.class);
+    ResponseEntity<String> response = restTemplate.getForEntity("/users", String.class);
+//    ResponseEntity<String> response4 = restTemplate.getForEntity("http://localhost:" + port + "/api/v1/users", String.class);
+//    ResponseEntity<String> response2 = restTemplate.getForEntity("/actuator/mappings", String.class);
+//    ResponseEntity<String> response3 = restTemplate.getForEntity("/actuator/health", String.class);
+
+//    var responseEntity = get(Uri.USERS);
+
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
   }
 
-  protected ResponseEntity get(ControllerConstant.Uri uri) {
-
-    HttpEntity<String> stringHttpEntity = new HttpEntity<>(null, null);
-
-    return restTemplate.exchange(
-        URI.create(uri.value), HttpMethod.GET, stringHttpEntity, String.class
-//        URI.create("/api/v1/users"), HttpMethod.GET, stringHttpEntity, String.class
-//        URI.create("/users"), HttpMethod.GET, stringHttpEntity, String.class
-    );
-  }
+//  protected ResponseEntity get(ControllerConstant.Uri uri) {
+//
+//    HttpEntity<String> stringHttpEntity = new HttpEntity<>(null, null);
+//
+//    return restTemplate.exchange(
+//        URI.create(uri.value), HttpMethod.GET, stringHttpEntity, String.class
+////        URI.create("/api/v1/users"), HttpMethod.GET, stringHttpEntity, String.class
+////        URI.create("http://localhost:" + port + "/api/v1/users"), HttpMethod.GET, stringHttpEntity, String.class
+//    );
+//  }
 }
