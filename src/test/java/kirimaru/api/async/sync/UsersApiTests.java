61301ec0364c841d43c18bb3bf5bbf271c63205d
水上 皓登 <nainaistar@gmail.com>
2023-05-09 23:59:25 +0900

    annotationを最適な状態に変更

--- src/test/java/kirimaru/api/async/sync/UsersApiTests.java
+++ src/test/java/kirimaru/api/async/sync/UsersApiTests.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Answers;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -16,7 +17,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 
-@WebMvcTest(Users2Api.class)
+@WebFluxTest(Users2Api.class)
 //@AutoConfigureWebClient
 class Users2ApiTests {
 
